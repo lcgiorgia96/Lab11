db1b59702e9edbf021f33e25ff42a3f916feac02
giorgialacava <giorgialacava@mbp-di-giorgia.homenet.telecomitalia.it>
2019-05-31 18:27:54 +0200

    .

--- src/it/polito/tdp/bar/model/TestModel.java
+++ src/it/polito/tdp/bar/model/TestModel.java
@@ -5,13 +5,7 @@ public class TestModel {
 	public static void main(String[] args) {
 		
 		Simulatore sim= new Simulatore();
-		for (int E=0; E<2000;E++) {
-			sim.init();
-			
-			
-			
-			
-		}
+sim.init();
 sim.run();
 System.out.println("TOT CLIENTI: "+sim.getNumero_totale_clienti()+"\n Numero clienti soddisfatti: "+sim.getNumero_clienti_soddisfatti()+"\nNumero clienti insoddisfatti: "+sim.getNumero_clienti_insoddisfatti());
 	}
